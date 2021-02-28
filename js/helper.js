44be632b6c838da90def5c9b29cc68b515c15e90
normaloide <edoardoalejandroferrari@gmail.com>
2021-02-28 19:04:20 +0100

    Clear code
    Made code more readable and added folders

---
+++ js/helper.js
@@ -0,0 +1,5 @@
+if ( window.screen.width < 960 ) {
+    alert( "This website is not intended for "
+        + window.screen.width
+        + "px monitors. Try to flip your device horizontally and see if the problem persists" );
+}
