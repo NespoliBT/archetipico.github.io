7c942f1c7abc266f74c9cd0bc41053aac01f8df5
normaloide <edoardoalejandroferrari@gmail.com>
2021-03-04 18:01:04 +0100

    Fix sizes
    Tried to fix some sizes and catched an uncaught exception

--- js/main.js
+++ js/main.js
@@ -32,20 +32,24 @@ function initializer() {
 
 /* Sets colors for light and dark mode */
 function setColors(mode) {
-    if ( mode === "light" ) {
-        document.querySelector(":root").style.setProperty("--content-background", "#fcfcfc");
-        document.querySelector(":root").style.setProperty("--content-link", "#2f00ffe3");
-        document.querySelector(":root").style.setProperty("--menu-background", "#2f00ffe3");
-        document.querySelector(":root").style.setProperty("--content-text-color", "#141414");
+    try {
+        if ( mode === "light" ) {
+            document.querySelector(":root").style.setProperty("--content-background", "#fcfcfc");
+            document.querySelector(":root").style.setProperty("--content-link", "#2f00ffe3");
+            document.querySelector(":root").style.setProperty("--menu-background", "#2f00ffe3");
+            document.querySelector(":root").style.setProperty("--content-text-color", "#141414");
 
-        document.getElementById("dark-mode-button").innerHTML ="Dark Mode";
-    } else {
-        document.querySelector(":root").style.setProperty("--content-background", "#141414");
-        document.querySelector(":root").style.setProperty("--content-link", "#fda2a2e3");
-        document.querySelector(":root").style.setProperty("--content-text-color", "#fcfcfc");
-        document.querySelector(":root").style.setProperty("--menu-background", "#fda2a2e3");
+            document.getElementById("dark-mode-button").innerHTML ="Dark Mode";
+        } else {
+            document.querySelector(":root").style.setProperty("--content-background", "#141414");
+            document.querySelector(":root").style.setProperty("--content-link", "#fda2a2e3");
+            document.querySelector(":root").style.setProperty("--content-text-color", "#fcfcfc");
+            document.querySelector(":root").style.setProperty("--menu-background", "#fda2a2e3");
 
-        document.getElementById("dark-mode-button").innerHTML = "Light Mode";
+            document.getElementById("dark-mode-button").innerHTML = "Light Mode";
+        }
+    } catch (e) {
+        console.log("Dark Mode Button does not exist on this html");
     }
 }
 
@@ -53,10 +57,14 @@ function setColors(mode) {
 function setSizes(device) {
     if ( device === "desktop" ) {
         document.querySelector(":root").style.setProperty("--content-text-size", "1.3rem");
+        document.querySelector(":root").style.setProperty("--menu-hovered-width", "17%");
+        document.querySelector(":root").style.setProperty("--menu-initial-width", "7%");
         document.querySelector(":root").style.setProperty("--menu-padding-left", "1.7rem");
         document.querySelector(":root").style.setProperty("--menu-text-size", "1.2rem");
     } else {
         document.querySelector(":root").style.setProperty("--content-text-size", "0.8rem");
+        document.querySelector(":root").style.setProperty("--menu-hovered-width", "21%");
+        document.querySelector(":root").style.setProperty("--menu-initial-width", "7%");
         document.querySelector(":root").style.setProperty("--menu-padding-left", "1rem");
         document.querySelector(":root").style.setProperty("--menu-text-size", "0.7rem");
     }
