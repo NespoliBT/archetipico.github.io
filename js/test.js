0d4d695c598102f43bd242608250494038cb44bd
normaloide <edoardoalejandroferrari@gmail.com>
2021-03-07 00:13:50 +0100

    Add wireframe
    Made a better cube on screen, modified the scene background

--- js/test.js
+++ js/test.js
@@ -1,16 +1,19 @@
-/**
- * Test function from the THREE.js documentation
- */
-function main() {
+function draw() {
+    const bgColor = document.querySelector(":root").style.getPropertyValue("--content-link");
+    const color = document.querySelector(":root").style.getPropertyValue("--content-text-color");
+    const height = document.getElementById("scene").clientHeight;
+    const width = document.getElementById("scene").clientWidth;
+    
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, document.getElementById('scene').clientWidth / document.getElementById('scene').clientHeight, 0.1, 1000 );
+    const camera = new THREE.PerspectiveCamera( 25, width / height, 0.1, 1000 );
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( document.getElementById('scene').clientWidth, document.getElementById('scene').clientHeight );
-    document.getElementById('scene').appendChild( renderer.domElement );
+    const renderer = new THREE.WebGLRenderer( { alpha: true } );
+    renderer.setSize( width, height );
+    renderer.setClearColor( bgColor.substring(0, 7), 0.05 );
+    document.getElementById("scene").appendChild( renderer.domElement );
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshBasicMaterial( { color: color, wireframe: true } );
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
@@ -19,8 +22,8 @@ function main() {
     const animate = function () {
         requestAnimationFrame( animate );
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        cube.rotation.x += 0.005;
+        cube.rotation.y += 0.005;
 
         renderer.render( scene, camera );
     };
