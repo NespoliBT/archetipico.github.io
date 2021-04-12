f5bf076a0c091f0a0bc82127a498f4ddddd2dad6
NespoliBT <40525058+NespoliBT@users.noreply.github.com>
2021-04-12 21:34:14 +0200

    :bug: fix dark mode

--- js/main.js
+++ js/main.js
@@ -13,40 +13,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
   themeSwitch.addEventListener("click", function () {
     console.log(document.cookie);
-    if (document.cookie.includes("theme=dark")) {
-      document.cookie = "theme=light";
-    } else {
+    if (document.cookie.includes("theme=light")) {
       document.cookie = "theme=dark";
+    } else {
+      document.cookie = "theme=light";
     }
-    themeSwitch.innerHTML = document.cookie.includes("theme=dark") ? "" : "";
+    themeSwitch.innerHTML = document.cookie.includes("theme=light") ? "" : "";
     setTheme();
   });
 
-  clearWebStorage.addEventListener("click", function () {
-    localStorage.clear();
-    sessionStorage.clear();
+  clearWebStorage &&
+    clearWebStorage.addEventListener("click", function () {
+      localStorage.clear();
+      sessionStorage.clear();
 
-    clearWebStoragePopup.classList.add("open");
+      clearWebStoragePopup.classList.add("open");
 
-    setTimeout(() => {
-      clearWebStoragePopup.classList.remove("open");
-    }, 2000);
-  });
+      setTimeout(() => {
+        clearWebStoragePopup.classList.remove("open");
+      }, 2000);
+    });
 
-  themeSwitch.innerHTML = document.cookie.includes("theme=dark") ? "" : "";
+  themeSwitch.innerHTML = document.cookie.includes("theme=light") ? "" : "";
   setTheme();
 });
 
 function setTheme() {
-  if (document.cookie.includes("theme=dark")) {
-    document.documentElement.style.setProperty("--color-1", "#141414");
-    document.documentElement.style.setProperty("--color-2", "#242424");
-    document.documentElement.style.setProperty("--color-3", "#fda0a0");
-    document.documentElement.style.setProperty("--color-4", "#fcfcfc");
-  } else {
+  if (document.cookie.includes("theme=light")) {
     document.documentElement.style.setProperty("--color-1", "#fcfcfc");
     document.documentElement.style.setProperty("--color-2", "#eeeeee");
     document.documentElement.style.setProperty("--color-3", "#2f00ff");
     document.documentElement.style.setProperty("--color-4", "#141414");
+  } else {
+    document.documentElement.style.setProperty("--color-1", "#141414");
+    document.documentElement.style.setProperty("--color-2", "#242424");
+    document.documentElement.style.setProperty("--color-3", "#fda0a0");
+    document.documentElement.style.setProperty("--color-4", "#fcfcfc");
   }
 }
